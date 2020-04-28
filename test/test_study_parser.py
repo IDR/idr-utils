360c367de0d7288737721437ea9474aaafe87b40
Sebastien Besson <seb.besson@gmail.com>
2020-04-28 15:14:56 +0100

    Add test for valid DOIs

--- test/test_study_parser.py
+++ test/test_study_parser.py
@@ -6,6 +6,13 @@ import pytest
 
 class TestDOI(object):
 
+    VALID_DOIS = [
+      '10.1083/jcb.20110809',
+      '10.1016/j.cell.2008.12.041',
+      '10.17867/10000108',
+      '10.1093/gigascience/giw014'
+    ]
+
     def test_missing_key(self):
         assert StudyParser.parse_data_doi({}, 'Study DOI') == {}
 
@@ -23,6 +30,12 @@ class TestDOI(object):
         d = {'Study DOI': 'doi.org/10.17867/10000134'}
         with pytest.raises(Exception):
             StudyParser.parse_data_doi(d, 'Study DOI')
+
+    @pytest.mark.parametrize('doi', VALID_DOIS)
+    def test_valid_dois(self, doi):
+        d = {'Study DOI': 'https://doi.org/%s' % doi}
+        assert StudyParser.parse_data_doi(d, 'Study DOI') == {
+            'Data DOI': doi}
 
     def test_doi_name(self):
         d = {'Study DOI': '10.17867/10000134'}
